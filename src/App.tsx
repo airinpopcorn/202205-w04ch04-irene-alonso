27a0a0b527d4a18b0b5ef3bc6ab36456456ed521
Irene <iavivas3@gmail.com>
2022-06-02 20:44:52 +0200

    Create Actions component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css';
 import React from 'react';
 import { Keyboard } from './components/Keyboard';
+import { Actions } from './components/Actions';
 
 export function App() {
   return (
     <div className="App">
-      <Keyboard></Keyboard>
+      <div className="container">
+        <span className="message">Calling...</span>
+        <main className="phone">
+          <div className="keyboard-container">
+            <Keyboard></Keyboard>
+          </div>
+          <div className="actions">
+            <Actions></Actions>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Actions.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Actions() {
+  return (
+    <>
+      <span className="number">667359961</span>
+      <a href="#" className="call">
+        Call
+      </a>
+      <a href="#" className="hang active">
+        Hang
+      </a>
+    </>
+  );
+}
