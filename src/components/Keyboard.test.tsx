6a97d950f039d7cc04c4a0996033b05266bdaacf
Irene <iavivas3@gmail.com>
2022-06-02 20:17:46 +0200

    Add Keyboard test

---
+++ src/components/Keyboard.test.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+import { render, screen } from '@testing-library/react';
+import { Keyboard } from './Keyboard';
+
+describe('Given the component Keyboard', () => {
+  const mockKeyboard = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0',
+    'delete',
+  ];
+
+  describe('When calling it with a Keyboard', () => {
+    test('Then it should be rendered', () => {
+      // Arrange
+      render(<Keyboard item={mockKeyboard} />);
+
+      // Assert
+      expect(screen.getAllByRole('listitem')).toHaveLength(mockKeyboard.length);
+    });
+  });
+});
